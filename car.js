57a510d44e90f1cfafb0de2afd7cca2ee143609d
Kriston-12 <2574992107@qq.com>
2024-01-06 17:14:54 -0500

    initial sensor

--- car.js
+++ car.js
@@ -9,10 +9,17 @@ class Car {
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
+
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update() {
+        this.#move();
+        this.sensor.update();
+    }
+
+    #move() {
         if (this.controls.forward) {
             this.speed += this.acceleration;
         }
@@ -55,5 +62,7 @@ class Car {
         ctx.rect(-this.width/2, -this.height/2, this.width, this.height);
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
